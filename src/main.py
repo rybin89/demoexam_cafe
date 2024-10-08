14a83605d769bb9fd8e5385a013c751b2792ce34
rybin89 <rybin.y89@mail.ru>
2024-10-08 12:13:39 +0500

    Предстовления

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("sadadsasd")
+from Views.login import *
+from Controllers.UserController import *
+
+import sys
+app = QtWidgets.QApplication(sys.argv)
+login = QtWidgets.QMainWindow()
+ui = Ui_login()
+ui.setupUi(login)
+login.show()
+sys.exit(app.exec_())
